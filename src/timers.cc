0c49cfb763e7e63bdf232dc67d7821c5d89ca8d4
Yagiz Nizipli <yagiz@nizipli.com>
2024-11-18 10:37:42 -0500

    test: add fast api tests for getLibuvNow()

--- src/timers.cc
+++ src/timers.cc
@@ -1,4 +1,7 @@
 #include "timers.h"
+
+#include <node_debug.h>
+
 #include "env-inl.h"
 #include "node_external_reference.h"
 #include "util-inl.h"
@@ -35,6 +38,7 @@ void BindingData::SlowGetLibuvNow(const FunctionCallbackInfo<Value>& args) {
 
 double BindingData::FastGetLibuvNow(Local<Object> unused,
                                     Local<Object> receiver) {
+  TRACK_V8_FAST_API_CALL("timers.getLibuvNow");
   return GetLibuvNowImpl(FromJSObject<BindingData>(receiver));
 }
 
